9394391e61ec57c46410ae689c294ed0f6924a51
mvzn-mohammed <124471277+mvzn-mohammed@users.noreply.github.com>
2024-10-24 21:31:33 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+
 
 function App() {
-  
+
+
+  return (
+    <h1 className="text-3xl font-bold underline bg-teal-300 flex justify-center align-middle">
+      its mazen!
+      
+  </h1>    
+   
+  )
 }
 
-export default App;
+export default App
